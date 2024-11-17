bfccf00056b4ab592e7b164d479b985c904d6c74
Pawel Cwik <clockworkjava01@gmail.com>
2024-11-17 10:27:48 +0100

    Tworzenie rezerwacji, krok drugi

--- src/main/java/pl/clockworkjava/gnomix/controllers/ReservationController.java
+++ src/main/java/pl/clockworkjava/gnomix/controllers/ReservationController.java
@@ -1,11 +1,17 @@
 package pl.clockworkjava.gnomix.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.clockworkjava.gnomix.domain.reservation.ReservationService;
+import pl.clockworkjava.gnomix.domain.room.Room;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 @RequestMapping("/reservations")
@@ -29,4 +35,17 @@ public class ReservationController {
         return "reservationStepOne";
     }
 
+    @PostMapping("/create/steptwo")
+    public String creationWizardStepTwo(
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate fromDate,
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate toDate,
+            int size,
+            Model m) {
+
+        List<Room> rooms = this.reservationService.getAvaiableRooms(fromDate, toDate, size);
+        m.addAttribute("rooms", rooms);
+        m.addAttribute("fromDate", fromDate);
+        m.addAttribute("toDate", toDate);
+        return "reservationStepTwo";
+    }
 }
--- src/main/java/pl/clockworkjava/gnomix/domain/reservation/ReservationService.java
+++ src/main/java/pl/clockworkjava/gnomix/domain/reservation/ReservationService.java
@@ -2,20 +2,31 @@ package pl.clockworkjava.gnomix.domain.reservation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.clockworkjava.gnomix.domain.room.Room;
+import pl.clockworkjava.gnomix.domain.room.RoomService;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class ReservationService {
 
     private ReservationRepository repository;
+    private RoomService roomService;
 
     @Autowired
-    public ReservationService(ReservationRepository repository) {
+    public ReservationService(
+            ReservationRepository repository,
+            RoomService roomService) {
         this.repository = repository;
+        this.roomService = roomService;
     }
 
     public List<Reservation> getAll() {
         return this.repository.findAll();
+    }
+
+    public List<Room> getAvaiableRooms(LocalDate from, LocalDate to, int size) {
+        return this.roomService.findAll();
     }
 }
