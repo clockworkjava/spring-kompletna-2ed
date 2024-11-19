567d73638e8df1b76243fa09e7cb5e6d4ffb5a83
Pawel Cwik <clockworkjava01@gmail.com>
2024-11-19 13:31:17 +0100

    REST Controller + postac zwracanych danych

---
+++ src/main/java/pl/clockworkjava/gnomix/controllers/api/RestRoomController.java
@@ -0,0 +1,34 @@
+package pl.clockworkjava.gnomix.controllers.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.clockworkjava.gnomix.controllers.dto.AvailableRoomDTO;
+import pl.clockworkjava.gnomix.domain.reservation.ReservationService;
+import pl.clockworkjava.gnomix.domain.room.Room;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+public class RestRoomController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public RestRoomController(ReservationService service) {
+        this.reservationService = service;
+    }
+
+    @GetMapping("api/getFreeRooms")
+    public List<AvailableRoomDTO> getAvailableRooms(
+            LocalDate from,
+            LocalDate to,
+            int size
+    ) {
+        return reservationService.getAvailableRooms(from, to, size).stream()
+                .map( room -> new AvailableRoomDTO(room.getNumber(), room.getId(), room.getBeds(), room.size()))
+                .toList();
+    }
+}
