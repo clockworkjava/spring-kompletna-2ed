8b4bbb6ee77d93dbd2631505572c31544c2f771b
Pawel Cwik <clockworkjava01@gmail.com>
2024-11-25 11:56:34 +0100

    Spring security - Role

--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -2,9 +2,16 @@ package pl.clockworkjava.gnomix.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,8 +24,31 @@ public class ApplicationSecurityConfig  {
                 .authorizeHttpRequests( http ->
                         http
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/rooms/**").hasAnyRole("MANAGER", "RECEPTION")
+                                .requestMatchers("/api/rooms/**").hasAnyRole("MANAGER")
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
+    }
+
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("pawelcwik")
+                .password(passwordEncoder().encode("pawelcwik"))
+                .roles("OWNER").build();
+
+        UserDetails user2 = User.builder()
+                .username("ali")
+                .password(passwordEncoder().encode("ali"))
+                .roles("RECEPTION").build();
+
+
+        return new InMemoryUserDetailsManager(user, user2);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
