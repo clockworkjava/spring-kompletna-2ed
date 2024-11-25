5ab6524833bbbc531979991a14bfa10daa21165a
Pawel Cwik <clockworkjava01@gmail.com>
2024-11-25 18:06:56 +0100

    Spring security - Zapisywanie uzytkownikow w bazie danych

--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -2,18 +2,14 @@ package pl.clockworkjava.gnomix.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity(debug = true)
@@ -33,24 +29,12 @@ public class ApplicationSecurityConfig  {
                 .formLogin(form -> form
                         .loginPage("/login")
                         .permitAll()
-                        .defaultSuccessUrl("/"));
+                        .defaultSuccessUrl("/rooms", true))
+                .logout(logout -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).permitAll()
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID"));
         return httpSecurity.build();
-    }
-
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("pawelcwik")
-                .password(passwordEncoder().encode("pawelcwik"))
-                .roles("MANAGER").build();
-
-        UserDetails user2 = User.builder()
-                .username("ali")
-                .password(passwordEncoder().encode("ali"))
-                .roles("RECEPTION").build();
-
-
-        return new InMemoryUserDetailsManager(user, user2);
     }
 
     @Bean
