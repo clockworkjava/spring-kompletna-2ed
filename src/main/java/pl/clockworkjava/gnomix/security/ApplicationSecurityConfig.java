bbab5c4d5b7446e0e98d8ce983440ce6d9fc60f8
Pawel Cwik <clockworkjava01@gmail.com>
2024-12-06 23:31:02 +0100

    CORS

--- src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/clockworkjava/gnomix/security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -31,13 +32,14 @@ public class ApplicationSecurityConfig  {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeHttpRequests( http ->
+                .authorizeHttpRequests(http ->
                         http
                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/error/**", "/error", "/login", "/", "/api/login", "/api/refreshtoken").permitAll()
                                 .anyRequest().authenticated())
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
-                                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         httpSecurity.addFilterBefore(new CustomAuthenticationFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
         httpSecurity.addFilterBefore(new CustomAuthorizationFilter(), CustomAuthenticationFilter.class);
